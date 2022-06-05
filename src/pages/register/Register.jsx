680be2fd8511158fc2435a2954ba1da14f733c14
2mnguyen1 <2mnguyen1@gmail.com>
2022-06-05 14:57:52 -0700

    adding register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,26 @@
-
+import "./register.css"
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className="login-page">
+        <div className="login-wrapper">
+            <div className="login-left">
+                <h3 className="login-logo">Minh's MặtSách</h3>
+                <span className="login-desc">
+                    Tìm bạn bốn phương ở thế giới của Minh
+                </span>
+            </div>
+            <div className="login-right">
+                <div className="login-box">
+                    <input placeholder="Username" className="login-input" type="text"/>
+                    <input placeholder="Email" className="login-input" type="email"/>
+                    <input placeholder="Password" className="login-input" type="password"/>
+                    <input placeholder="Password Again" className="login-input" type="password"/>
+                    <button className="login-btn">Sign Up</button>
+                    <button className="register-button">Log into Account</button>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
