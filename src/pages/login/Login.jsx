8ea0688a00c1ab2f9260852793b614c30cf881c3
2mnguyen1 <2mnguyen1@gmail.com>
2022-06-07 14:35:12 -0700

    adding login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,15 @@
 import "./login.css"
+import {useRef} from 'react'
+
 
 export default function Login() {
+    const email = useRef()
+    const password = useRef()
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        // console.log(email.current.value)
+    }
   return (
     <div className="login-page">
         <div className="login-wrapper">
@@ -11,13 +20,25 @@ export default function Login() {
                 </span>
             </div>
             <div className="login-right">
-                <div className="login-box">
-                    <input placeholder="Email" className="login-input" type="email"/>
-                    <input placeholder="Password" className="login-input" type="password"/>
+                <form className="login-box" onSubmit={handleSubmit}>
+                    <input
+                        placeholder="Email"
+                        className="login-input"
+                        type="email"
+                        ref={email}
+                        required
+                        />
+                    <input
+                        placeholder="Password"
+                        className="login-input"
+                        type="password"
+                        ref={password}
+                        required
+                        />
                     <button className="login-btn">Log In</button>
                     <span className="login-forgot">Forgot Password?</span>
                     <button className="register-button">Create a New Account</button>
-                </div>
+                </form>
             </div>
         </div>
     </div>
